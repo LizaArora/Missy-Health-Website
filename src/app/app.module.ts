197e882ec53e17c71a7f77a6cf6ad3162e3ebd40
Akshat Maheshwari <maheshwariakshat482@gmail.com>
2023-03-27 20:25:09 +0530

    added components routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { PostCardComponent } from './layouts/post-card/post-card.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { WhoAreWeComponent } from './pages/who-are-we/who-are-we.component';
+import { TermsAndConditionsComponent } from './pages/terms-and-conditions/terms-and-conditions.component';
+import { SinglePostComponent } from './pages/single-post/single-post.component';
+import { SingleCategoryComponent } from './pages/single-category/single-category.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SubscriptionFormComponent } from './subscription-form/subscription-form.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+    PostCardComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    BlogComponent,
+    WhoAreWeComponent,
+    TermsAndConditionsComponent,
+    SinglePostComponent,
+    SingleCategoryComponent,
+    HomeComponent,
+    SubscriptionFormComponent,
+    TestimonialsComponent
   ],
   imports: [
     BrowserModule,
